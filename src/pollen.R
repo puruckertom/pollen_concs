c743fc9e4425862850a219c563179e33cd186f13
sumathys <sumathy.sinnathamby@gmail.com>
2016-10-18 11:16:44 -0400

    Added CSVs

--- src/pollen.R
+++ src/pollen.R
@@ -1,4 +1,3 @@
-#sumathy was here
 
 read.csv(file.choose(),header=TRUE)->pollen ##load csv file contains pesticide name, pollen and residues
 
@@ -12,3 +11,17 @@ read.csv(file.choose(),header=TRUE)->concentration ##load csv file contains pest
 
 plot(pollen$Application, pollen$Nectar, main="Scatterplot", 
     xlab="Imidacloprid Application  rate (lb a.i./A)  ", ylab="Nectar (mg a.i./kg)", pch=19)##drawing catter plots
+
+dat <- read.csv(file.choose(),header=TRUE) ##load PollenData.csv file contains pesticide name, pollen and residues
+library(ggplot2)##import ggplot2
+head(dat)
+ggplot(dat, aes(x = NECT, y = PESTI)) +
+  geom_point(aes(color = CROP), size = 2)##Residue concentration in nectar for each crop 
+
+library(ggplot2)##import ggplot2
+ggplot(dat, aes(x = POLLEN, y = PESTI)) +
+  geom_point(aes(color = CROP), size = 2)##Residue concentration in pollen for each crop 
+
+library(ggplot2)##import ggplot2
+dat <- read.csv(file.choose(),header=TRUE) ##load NormData.csv file contains pesticide name, normalized pollen and residues
+ggplot(dat, aes(x = INT, y = RESI)) + geom_boxplot() ## Normalized residue concentration boxplot
